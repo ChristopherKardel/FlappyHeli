b75aac6f723f93623ba3ba120316e6867f041504
SerioeserGamer_ <106988163+ChristopherKardel@users.noreply.github.com>
2024-03-25 20:00:41 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+error_reporting(E_ERROR | E_PARSE); //Keine Warnings anzeigen
+
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$db = "flappydatabank"; // Datenbank Name
+
+// Verbindung herstellen
+$mysqli = new mysqli($servername, $username, $password, $db);
+
+// Überprüfen, ob die Verbindung erfolgreich war
+if ($mysqli->connect_error) {
+    die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
+}
+
+$register = $_POST['register'];
+$update_score = $_POST['update_score'];
+$username = $_POST['username'];
+$highscore = $_POST['highscore'];
+$new_highscore = $_POST['new_highscore'];
+$id = $_POST['id'];
+ 
+
+
+if($register == "1"){
+    $querry = "INSERT INTO userdata (Username, Highscore) VALUES ('" . $username . "','" . $highscore . "');";
+    //Send zur Datenbank und lasse eintragen
+    $result = $mysqli->query($querry);
+    if(!$result) {
+        printf("%s\n", $mysqli -> error);
+        echo("L");
+        exit();
+    }else{
+        
+        
+        //Gib ID Raus
+        echo $mysqli -> insert_id . "\t";
+    }
+
+
+}else if($update_score == "1") {
+    //$querry = "UPDATE userdata SET Highscore = '" . $new_highscore . "' WHERE id = '" . $id . "';";
+    $querry = "UPDATE `userdata` SET `Highscore` = '" . $new_highscore . "' WHERE `userdata`.`id` = '" . $id . "';";
+    //$querry = "UPDATE `userdata` SET `Highscore` = '" . $_POST['new_highscore'] . "' WHERE `userdata`.`id` = '" . $_POST['id'] . "';";
+    //$querry = "UPDATE `userdata` SET `Highscore` = '1000' WHERE `userdata`.`id` = 64;";
+    //Sende zur Datenbank und lasse eintragen
+    $result = $mysqli->query($querry);
+    if (!$result) {
+        printf("%s\n", $mysqli -> error);
+        echo("L");
+        exit();
+    }else{
+        echo("S");
+    }
+}
+?>
